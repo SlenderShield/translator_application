8c2d6c495c13db4fdacf78ac200e72e0f899a675
Muralidhara Bhat K S <ksmuralidhara0@gmail.com>
2022-10-15 21:52:46 +0530

    copy and speech functions

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@ const selectTag = document.querySelectorAll('select'),
     inputText = document.querySelector('.input-text'),
     outputText = document.querySelector('.output-text'),
     exchangeBtn = document.querySelector('.exchange'),
-    translateBtn = document.querySelector('button')
+    translateBtn = document.querySelector('button'),
+    icons = document.querySelectorAll('.controls i')
+
 
 selectTag.forEach((elem, id) => {
     for (let country_code in countries) {
@@ -10,7 +12,7 @@ selectTag.forEach((elem, id) => {
         let newOption;
         if (id == 0 && country_code == 'en-GB') {
             selected = "selected"
-            newOption = `<option value="${country_code}" ${selected}>${countries[country_code]}</option>`;
+            newOption = `<option value="${country_code}" ${selected}>${countries[country_code]}</optio4n>`;
         } else if (id == 1 && country_code == 'kn-IN') {
             selected = "selected"
             newOption = `<option value="${country_code}" ${selected}>${countries[country_code]}</option>`;
@@ -34,11 +36,34 @@ exchangeBtn.addEventListener("click", () => {
 
 translateBtn.addEventListener("click", () => {
     let toTranslate = inputText.value
-    fromLang = selectTag[0].value
-    toLang = selectTag[1].value
+    let fromLang = selectTag[0].value
+    let toLang = selectTag[1].value
     let api = `https://api.mymemory.translated.net/get?q=${toTranslate}!&langpair=${fromLang}|${toLang}`
     fetch(api).then(res => res.json()).then(data =>
         // console.log(data)
         outputText.value = data.responseData.translatedText
     )
+})
+
+
+icons.forEach(icons => {
+    icons.addEventListener('click', ({ target }) => {
+        if (target.classList.contains('fa-copy')) {
+            if (target.classList.contains('from-txt')) {
+                navigator.clipboard.writeText(inputText.value)
+            } else {
+                navigator.clipboard.writeText(outputText.value)
+            }
+        } else {
+            let utterance;
+            if (target.classList.contains('from-txt')) {
+                utterance = new SpeechSynthesisUtterance(inputText.value)
+                utterance.lang = selectTag[0].value
+            } else {
+                utterance = new SpeechSynthesisUtterance(outputText.value)
+                utterance.lang = selectTag[1].value
+            }
+            speechSynthesis.speak(utterance)
+        }
+    })
 })
